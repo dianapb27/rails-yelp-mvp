44bb87a8081966649678aff362ccc7358dafdced
Diana Pinon <dianapinonb@gmail.com>
2020-11-11 14:20:32 -0700

    Created yelp MVP

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :restaurants, only: [:index, :show, :new, :create] do
+    # member do
+    #   get :reviews
+    # end
+    resources :reviews, only: [ :new, :create ]
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Generating fake restaurants"
+Restaurant.destroy_all
+
+8.times do
+  Restaurant.create!(
+    name: Faker::TvShows::BreakingBad.episode,
+    address: Faker::Address.street_address,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample(),
+    phone_number: Faker::PhoneNumber.cell_phone
+  )
+end
+
+puts "All done!"
+
